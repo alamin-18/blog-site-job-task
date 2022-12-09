e951af44869f7976656dcf979fff3e0c91fefa35
Md Al Amin <fpalamin6@gmail.com>
2022-12-09 12:34:27 +0600

    nevber added

--- src/Pages/Home/Home/Navber/Navber.jsx
+++ src/Pages/Home/Home/Navber/Navber.jsx
@@ -2,32 +2,17 @@ import React from 'react';
 
 const Navber = () => {
     return (
-        <div className="navbar bg-base-100">
-            <div className="flex-1">
-                <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
+        <div className="navbar justify-between  bg-base-100">
+            <div className="">
+                <a href='/' className="btn btn-ghost normal-case text-xl"><span className='text-[#27A365]'>ATC.</span><span>WORLD</span></a>
             </div>
-            <div className="flex-none gap-2">
-                <div className="form-control">
-                    <input type="text" placeholder="Search" className="input input-bordered" />
-                </div>
-                <div className="dropdown dropdown-end">
-                    <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
-                        <div className="w-10 rounded-full">
-                            <img src="https://placeimg.com/80/80/people" />
-                        </div>
-                    </label>
-                    <ul tabIndex={0} className="mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52">
-                        <li>
-                            <a className="justify-between">
-                                Profile
-                                <span className="badge">New</span>
-                            </a>
-                        </li>
-                        <li><a>Settings</a></li>
-                        <li><a>Logout</a></li>
-                    </ul>
-                </div>
+            <div className="">
+                <input type="text" placeholder="Search" className="rounded-full border-2 h-10 w-96 " />
             </div>
+            <div className="l">
+                <h1>Create accout</h1>
+            </div>
+
         </div>
     );
 };
