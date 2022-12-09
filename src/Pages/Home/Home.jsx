4873f59907f9fb33ddadd1b4c4d5596ae0c5d50f
Md Al Amin <fpalamin6@gmail.com>
2022-12-09 12:14:27 +0600

    banner added

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import Banner from './Home/Banner/Banner';
+import Navber from './Home/Navber/Navber';
 
 const Home = () => {
     return (
         <div>
+            <Navber></Navber>
             <Banner></Banner>
         </div>
     );
--- src/Pages/Home/Home/Banner/Banner.jsx
+++ src/Pages/Home/Home/Banner/Banner.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 
 const Banner = () => {
     return (
-        <div className="hero min-h-screen" style={{ backgroundImage: `url("https://placeimg.com/1000/800/arch")` }}>
+        <div className="hero  " style={{ backgroundImage: `url("https://cdn.pixabay.com/photo/2017/11/27/21/31/computer-2982270_960_720.jpg")` }}>
             <div className="hero-overlay bg-opacity-60"></div>
-            <div className="hero-content text-center text-neutral-content">
-                <div className="max-w-md">
-                    <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
-                    <p className="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-                    <button className="btn btn-primary">Get Started</button>
+            <div className=" mt-96 mb-10  text-neutral-content">
+                <div className="">
+                    <h1 className="mb-5 text-5xl font-bold">Computer Engineering</h1>
+                    <p className="mb-5">142,765 Computer Engineers follow this.</p>
+                    
                 </div>
             </div>
         </div>
